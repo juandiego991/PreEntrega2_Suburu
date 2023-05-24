6594b4a481c188cda56b4e1f049ce75690536ad4
Juan Diego Suburú Salgado <juandiegosuburu@gmail.com>
2023-05-24 06:50:46 -0300

    Segunda carga a repo

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-                    // CALCULADORA DE PRESTAMOS - PAGOS EN CUOTAS (hasta 10 cuotas)
+// CALCULADORA DE PRESTAMOS - PAGOS EN CUOTAS (hasta 10 cuotas)
 
-                    // el interes mensual de este prestamo es 15%
-                    // El prestamo puede repagarse hasta en 10 cuotas
-
-
+// el interes mensual de este prestamo es 15%
+// El prestamo puede repagarse hasta en 10 cuotas
 
 
-                    //ingreso de datos para los calculos
-// let monto_solicitado = prompt("ingrese el monto total solicitado");
-// let cantidad_cuotas = prompt("ingrese la cantidad de cuotas en que desea repagar el prestamo (maximo 10)");
 
-
-// monto_solicitado = parseInt(monto_solicitado);
-// cantidad_cuotas = parseInt(cantidad_cuotas);
-
-
-                    //defino variable total_prestamo para agrupar los calculos y mostrar al usuario el primer output
+//defino variable total_prestamo para agrupar los calculos y mostrar al usuario el primer output
 // let total_prestamo = monto_solicitado + interes (monto_solicitado, cantidad_cuotas) * cantidad_cuotas;
-
-// console.log("el monto a pagar total es: ", total_prestamo );
 
 
 
@@ -81,28 +69,13 @@ console.log(lista_prestamos);
 
 
 
-                        // ciclo: para listar la cantidad de cuotas y el precio pagado por cuota con interes
-// for (let i = 0; i < cantidad_prestamos; i = i + 1 ){
-    
-//     let valor_cuota = monto_solicitado / cantidad_cuotas + interes(monto_solicitado, cantidad_cuotas) ;
-    
-//     if(cantidad_cuotas <= 10 && cantidad_cuotas >= 1 && monto_solicitado > 0 ){
-//         console.log("el monto de la cuota ", i + 1, "es: ", valor_cuota);
-//     }
-    
-//     else{
-//         console.log("");
-//     } 
-
-// }
-
-
+//Buscar un prestamo de la base de prestamos registrados en el arreglo lista_prestamos
 function buscar_prestamo (obj_prestamo){
     return obj_prestamo.usuario == prompt("ingrese el nombre del usuario para el que desea ver el prestamo");
 }
 
 let resultado_busqueda = lista_prestamos.find(buscar_prestamo);
-console.log(lista_prestamos.usuario, "ha tomado un prestamo de: ", resultado_busqueda);
+console.log("El usuario seleccionado ha tomado un prestamo de: ", resultado_busqueda);
 
 
 
